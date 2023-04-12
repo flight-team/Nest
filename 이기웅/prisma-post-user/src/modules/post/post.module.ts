f9b9a1b1d423dc7c0542ca534ec2d6985a4d6e3d
rldnd <gi981226@gmail.com>
2023-04-12 11:06:28 +0900

    fix(prisma-module): module to global module

--- 이기웅/prisma-post-user/src/modules/post/post.module.ts
+++ 이기웅/prisma-post-user/src/modules/post/post.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PostService } from './post.service';
-import { PostController } from './post.controller';
-import { PrismaModule } from '@/database/prisma.module';
 import { UserModule } from '../user/user.module';
+import { PostController } from './post.controller';
+import { PostService } from './post.service';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [UserModule],
   controllers: [PostController],
   providers: [PostService],
 })
